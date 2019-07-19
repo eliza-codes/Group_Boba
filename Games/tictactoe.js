8c873eea955aa2f2205f7508a8309e49a46f5059
eliza-codes <elizabethspygt@gmail.com>
2019-07-19 12:54:54 -0700

    making an unsucessful tic tact toe game

---
+++ Games/tictactoe.js
@@ -0,0 +1,20 @@
+const playerName = "First";
+const playerName2 = "Second";
+const player1 = "X";
+const player2 = "O";
+
+const playerOne{
+    var name: playerName;
+    var marker: player1;
+
+}
+
+const playerTwo{
+    var name2: playerName2;
+    var marker2: player2;
+
+}
+
+function printName (name){
+    console.log(playerName);
+}
