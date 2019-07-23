652b37bcca7c2c1fa7727bed696dad81e0fcb5ea
CloudlyC <Cloudlyc829@gmail.com>
2019-07-23 09:44:16 -0700

    worked on a lot of clicker files

--- Demo/ClickerGame.js
+++ Demo/ClickerGame.js
@@ -1,12 +1,6 @@
 var lives = 3;
 var score = 0;
 
-document.getElementById('clickboba').onclick = function changeposition() {
-  document.getElementById('clickboba').innerHTML = Math.floor(Math.random() * 1000);
-  getRandomPosition();
-}
-
-
 for (lives = 3; lives > -1; lives--) {
   displayLives(lives);
   console.log(lives);
@@ -25,46 +19,8 @@ function displayScore(score){
   scorecount.innerHTML = " SCORE: " + score;
 }
 
+ 
 
 
-/*
-onEvent("start_button", "click", function() {
-  setScreen("game_screen");
-  score = 0;
-  setText("total_score", score);
-  lives = 3;
-  setText("number_lives", lives);
-});
-var score = 0;
-onEvent("banana", "click", function() {
-  score = score + 1;
-  setText("total_score", score);
-  setPosition("banana", randomNumber(50,280), randomNumber(50, 350));
-  if (score==10) {
-    setScreen("win_screen");
-    setText("win_label", "You win!!!");
-  }
-});
-onEvent("playAgain_button", "click", function() {
-  setScreen("welcome_screen");
-});
-onEvent("tryAgain_button", "click", function() {
-  setScreen("welcome_screen");
-  score = 0;
-  setText("total_score", score);
-  lives = 3;
-  setText("number_lives", lives);
-});
 
 
-var lives = 3;
-onEvent("image1", "click", function(){
-lives = lives-1 ;
-setText("number_lives", lives);
-if (lives == 0) {
-  setScreen("lose_screen");
-  setText("lose_label", "You Lose!!!");
-}
-});
-
-*/
