92a567bfc1caef345d274195e1436955c76158cd
CloudlyC <Cloudlyc829@gmail.com>
2019-07-26 13:22:18 -0700

    complete

--- Cloudly/ClickerGame.js
+++ Cloudly/ClickerGame.js
@@ -1,13 +1,19 @@
 var lives = 3;
 var score = 0;
+var finish = false;
+var timelimit = 60;
+displayScore(score);
+timer(0);
 
-for (lives = 3; lives > -1; lives--) {
-  displayLives(lives);
-  console.log(lives);
+
+function displayLives(lives) {
+	livecount.innerHTML = " LIVES: " + lives;
 }
 
-function displayLives(lives){
-  livecount.innerHTML = " LIVES: " + lives;
+function timer(time) {
+	if (finish == false){
+	timercount.innerHTML = "TIMER: " + Math.ceil(timelimit - time);
+	}
 }
 
 /*for (score = 0; score < 150; score++){
@@ -15,19 +21,28 @@ function displayLives(lives){
   console.log(score);
 } */
 
-function displayScore(score){
-  scorecount.innerHTML = " SCORE: " + score;
+function displayScore(score) {
+	scorecount.innerHTML = " SCORE: " + score;
 }
-
+/*
 function onMouseDown(event){
-	var mouse = event.downPoint;
+	var mouse = event.point;
 	console.log("x equals = " + mouse.x + " y equals = " + mouse.y);
-	//return mouse.x;
+	if(mouse.x >= 30 && mouse.x <= 90 && mouse.y >= path.position.y-30 && mouse.y <= path.position.y+30){
+		score++;
+		displayScore(score);
+		path.remove();
+	} else if(mouse.x >= 70 && mouse.x <= 130 && mouse.y >= c1.position.y-70 && mouse.y <= c1.position.y+70){
+		score++;
+		displayScore(score);
+		c1.remove();
+	}	
 
 	
 }
+*/
 
-var boba = 10;
+var boba = 150;
 
 var path = new Path.Circle({
 	center: [60, 60],
@@ -36,20 +51,31 @@ var path = new Path.Circle({
 	strokeColor: 'black'
 });
 
-var c1 = new Path.Circle({
-	center: [100, 100],
-	radius: 30,
-	fillColor: "#000000",
-	strokeColor: 'black'
-});
 
-var c2 = new Path.Circle({
-	center: [200, 200],
-	radius: 30,
-	fillColor: "#000000",
-	strokeColor: 'black'
-});
 
+function onMouseDown(event) {
+	//var mouse = event.point;
+	if (finish == false) {
+		for (var i = 0; i < boba; i++) {
+			var item = project.activeLayer.children[i];
+
+			if (event.point.x >= item.position.x - 30 && event.point.x <= item.position.x + 30 && event.point.y >= item.position.y - 30 && event.point.y <= item.position.y + 30) {
+				score++;
+				displayScore(score);
+				item.position = [1500, 10];
+			}
+			else {
+				lives--;
+			}
+		}
+	}
+
+}
+
+
+
+
+/*
 function onFrame(event) {
 	
 		path.position.y += path.bounds.height / 20;
@@ -60,39 +86,36 @@ function onFrame(event) {
 		if (c1.bounds.top > view.size.height) {
 			c1.position.y = -c1.bounds.height;
 		}
-		c2.position.y += c2.bounds.height / 20;
-		if (c2.bounds.top > view.size.height) {
-			c2.position.y = -c2.bounds.height;
-		}
+		
 } 
+*/
 
 
-/*
 var symbol = new Symbol(path);
 
 
 for (var i = 0; i < boba; i++) {
-	
+
 	var center = Point.random() * view.size;
 	var placedSymbol = symbol.place(center);
 	//placedSymbol.scale(i / 2);
 }
 
 function onFrame(event) {
-	
-	for (var i = 0; i < boba; i++) {
-		var item = project.activeLayer.children[i];
-		item.position.y += item.bounds.height / 20;
-		
-		if (item.bounds.top > view.size.height) {
-			item.position.y = -item.bounds.height;
+	timer(event.time);
+	if (event.time >= timelimit) {
+		finish = true;
+	}
+	if (finish == false) {
+		for (var i = 0; i < boba; i++) {
+			var item = project.activeLayer.children[i];
+			item.position.y += item.bounds.height / 20;
+
+			if (item.bounds.top > view.size.height) {
+				item.position.y = -item.bounds.height;
+			}
 		}
 	}
-} 
-*/
-
-
-
-
+}
 
 
